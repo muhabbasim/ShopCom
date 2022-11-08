76b739a1ee2fcf84bb6b581afcfd748e4dad814e
Baim <mjnoonha90@gmail.com>
2022-11-08 20:41:24 +0300

    product details

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// // -------------------- product details js --------------------
+// let mainImg = document.getElementById('main-img');
+// let subImgs = document.getElementsByClassName('small-img');
+// console.log(mainImg, subImgs);
+
+// // trget the index number of each img 
+// subImgs[0].onClick = function() {
+//   mainImg.src = subImgs[0].src;
+// }
+// subImgs[1].onClick = function() {
+//   mainImg.src = subImgs[1].src;
+// }
+
+
+
+
+
+// for(var i = 0; i < subImgs.length; i++) {
+//   subImgs[i].addEventListener('click', () => {
+//     let img = subImgs[i].attributes.src;
+//     // let imgSrc = img.attributes.src
+//     console.log(imgSrc)
+//     // mainImg.src = subImgs[i].src;
+
+//   })
+// }
+
